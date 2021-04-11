236db8d9d534d1ed3bee9e81382d8b5a367ca488
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-04-10 22:56:51 -0700

    Update Part 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sdong027
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #3  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,28 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0x00; PORTB = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpA = 0x00;
+	unsigned char tmpB = 0x00;
+	unsigned char tmpC = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+	while(1) {
+		tmpA = PINA & 0xFF;	
+        tmpB = PINA & 0xFF;
+        tmpC = 0x00;
 
-    }
-    return 1;
+        for (int i = 0x00; i < 8; i++) {
+            if ((tmpA >> i) & 0x01) {
+                tmpC++;
+            }
+            if ((tmpB >> i) & 0x01) {
+                tmpC++;
+            }
+        }
+
+		PORTC = tmpC;
+	}
+	return 0;
 }
