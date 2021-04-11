eda116407a7e526495f27a704f7a443a9cefb319
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-04-11 00:16:16 -0700

    Completed part 3

---
+++ turnin/sdong027_lab3_part3_tests.py
@@ -0,0 +1,19 @@
+tests = [ {'description': '(Fuel Gauge Tested in Part 2) No Fuel (Low Fuel)',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0x40)],
+    },
+    {'description': 'Key In, Driver Absent',
+    'steps': [ {'inputs': [('PINA',0x10)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0x40)],
+    },
+    {'description': 'Key In, Driver In, Seatbelt In',
+    'steps': [ {'inputs': [('PINA',0x70)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0x40)],
+    },
+    {'description': 'Key In, Driver In, Seatbelt Not In',
+    'steps': [ {'inputs': [('PINA',0x30)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0xC0)],
+    },
+    ]
+#watch = ['PORTB']
+
